00cafe7f895793b58fb262706480510230fd3a0d
silpavutlapalli <silpavutlapalli19@gmail.com>
2022-07-28 17:31:20 +0530

    add comments

--- basics/dataset.py
+++ basics/dataset.py
@@ -4,4 +4,7 @@ name = "silpa"
 status = "learning python"
 tool = "PyCharam"
 message = name + " is " + status + " in " + tool
+#  returning status
 print(message)
+# TODO: add different examples for dataset
+
